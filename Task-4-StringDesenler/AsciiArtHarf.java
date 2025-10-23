621cc5166df9c5c48949838f222fa66fbba53f51
240541025 <240541025@firat.edu.tr>
2025-10-23 21:20:10 +0300

    AsciiArtHarf

---
+++ Task-4-StringDesenler/AsciiArtHarf.java
@@ -0,0 +1,14 @@
+public class ASCIIArt {
+    public static void main(String[] args) {
+        System.out.println("-------------------");
+        System.out.println("  M Harfi ASCII Art  ");
+        System.out.println("-------------------");
+        
+        // M harfinin ASCII Art çizimi
+        System.out.println("M   M");
+        System.out.println("MM MM");
+        System.out.println("M M M");
+        System.out.println("M   M");
+        System.out.println("M   M");
+    }
+}
