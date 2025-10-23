1d12ffc199877a6a19dafbb2676e75f500aece75
240541025 <240541025@firat.edu.tr>
2025-10-23 20:35:00 +0300

    JavaDeseni

---
+++ Task-1-HosgeldinMesajlari/JavaDeseni.java
@@ -0,0 +1,16 @@
+public class DesenCizme {
+    public static void main(String[] args) {
+        System.out.println("   J    A V     V  A   ");
+        System.out.println("   J   A A V   V  A A  ");
+        System.out.println("J  J  AAAAA V V  AAAAA ");
+        System.out.println(" JJ  A     A V  A     A");
+
+        // *Alternatif ve daha basit bir desen (eğer yukarıdaki çok karmaşıksa):
+        /*
+        System.out.println(" J A V A ");
+        System.out.println(" J A V A ");
+        System.out.println(" J A V A ");
+        System.out.println(" J A V A ");
+        */
+    }
+}
