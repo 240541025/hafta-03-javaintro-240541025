9faa771cac84571dc50cbc85d09e6a935abdbbb4
240541025 <240541025@firat.edu.tr>
2025-10-23 21:51:16 +0300

    AsciiArtIsim

---
+++ TASK-BONUS/AsciiArtIsim.java
@@ -0,0 +1,26 @@
+public class asciartiisim {
+    public static void main(String[] args) {
+        System.out.println("-----------------------------------------------------");
+        System.out.println("  MURAT İSMİ ASCII ART");
+        System.out.println("-----------------------------------------------------");
+
+        // M U R A T Harfleri
+
+        // Satır 1
+        System.out.println("M   M U   U RRRR  AAAAA TTTTT");
+
+        // Satır 2
+        System.out.println("MM MM U   U R   R A   A   T  ");
+
+        // Satır 3
+        System.out.println("M M M U   U RRRR  AAAAA   T  ");
+
+        // Satır 4
+        System.out.println("M   M U   U R R   A   A   T  ");
+
+        // Satır 5
+        System.out.println("M   M UUUUU R  RR A   A   T  ");
+
+        System.out.println("-----------------------------------------------------");
+    }
+}
