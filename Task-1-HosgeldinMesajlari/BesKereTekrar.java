d577faaa8b36db272411d96abf353db26cbd20a8
240541025 <240541025@firat.edu.tr>
2025-10-23 20:14:27 +0300

    BesKereTekrar

---
+++ Task-1-HosgeldinMesajlari/BesKereTekrar.java
@@ -0,0 +1,17 @@
+public class BesKereTekrar {
+    public static void main(String[] args) {
+        // Döngü kullanılarak 5 kere yazdırma (daha iyi bir yöntem)
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Java’ya Hoş Geldiniz");
+        }
+
+        // Veya döngü kullanmadan 5 kere alt alta yazılabilir:
+        /*
+        System.out.println("Java’ya Hoş Geldiniz");
+        System.out.println("Java’ya Hoş Geldiniz");
+        System.out.println("Java’ya Hoş Geldiniz");
+        System.out.println("Java’ya Hoş Geldiniz");
+        System.out.println("Java’ya Hoş Geldiniz");
+        */
+    }
+}
