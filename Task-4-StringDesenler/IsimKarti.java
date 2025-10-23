94f0c8b724cf3e220c125e1a5a0b7d54e6e6bdb1
240541025 <240541025@firat.edu.tr>
2025-10-23 21:17:05 +0300

    IsimKarti

---
+++ Task-4-StringDesenler/IsimKarti.java
@@ -0,0 +1,17 @@
+public class IsimKarti {
+    public static void main(String[] args) {
+        String ad = "Murat";
+        String soyad = "Hilaloğlu";
+        String ogrenciNo = "2024040512";
+        // Bölüm bilgisi güncellendi
+        String bolum = "Yazılım Mühendisliği"; 
+
+        System.out.println("---------------------------------------");
+        System.out.println("|             ÖĞRENCİ KARTI             |");
+        System.out.println("---------------------------------------");
+        System.out.println("| Ad Soyad    : " + ad + " " + soyad);
+        System.out.println("| Öğrenci No  : " + ogrenciNo);
+        System.out.println("| Bölüm       : " + bolum);
+        System.out.println("---------------------------------------");
+    }
+}
